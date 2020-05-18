b8e9ed88041ad98912f6e2fcb396e16561223703
Sanjeev-007 <53875709+Sanjeev-007@users.noreply.github.com>
2020-05-18 15:50:06 +0530

    Create sorting.java

---
+++ DAY-20/sorting.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] a = new int[n];
+        for(int a_i=0; a_i < n; a_i++){
+            a[a_i] = in.nextInt();
+        }
+        // Write Your Code Here
+        int ns=0;
+        for(int i=0;i<a.length-1;i++){
+            
+            for(int j=0;j<a.length-i-1;j++){
+                if(a[j]>a[j+1]){
+                    int o=a[j];
+                    a[j]=a[j+1];
+                    a[j+1]=o;
+                    ns++;
+                    
+                }
+            }
+        }
+        System.out.println("Array is sorted in "+ns+" swaps.");
+        System.out.println("First Element: "+a[0]);
+        System.out.println("Last Element: "+a[a.length-1]);
+    }
+}
