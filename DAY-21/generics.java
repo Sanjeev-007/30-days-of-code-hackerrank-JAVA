da23658f999b606bb74150ed21167c84f1f7f137
Sanjeev-007 <53875709+Sanjeev-007@users.noreply.github.com>
2020-05-18 15:50:46 +0530

    Create generics.java

---
+++ DAY-21/generics.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Printer <T> {
+
+    /**
+    *    Method Name: printArray
+    *    Print each element of the generic array on a new line. Do not return anything.
+    *    @param A generic array
+    **/
+    
+    // Write your code here
+    public void printArray(T[] a){
+        for(int i=0;i<a.length;i++){
+            System.out.println(a[i]);
+        }
+    }
+}
